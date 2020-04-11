d26264caedbcf7892caff4fb0671872e4012aae4
Thomas Gaston <thomasgaston11@gmail.com>
2020-04-11 11:29:46 -0600

    added routes and made some custom routes for rendering all artists/books/movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    resources :users do
+      resources :books
+    end
+  
+    resources :users do
+      resources :movies
+    end
+
+    resources :users do
+      resources :artists
+    end
+
+    get 'books/all_books', :to => 'books#all_books'
+    get 'movies/all_movies', :to => 'movies#all_movies'
+    get 'artists/all_artists', :to => 'artists#all_artists'
+
+  end
 end
