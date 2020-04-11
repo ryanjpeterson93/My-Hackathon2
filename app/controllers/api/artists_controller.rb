e2d95b17e84969ab30251405ba5f63ac8b358314
Thomas Gaston <thomasgaston11@gmail.com>
2020-04-11 10:21:01 -0600

    built out controllers, added has_many to user

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,10 +1,41 @@
 class Api::ArtistsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_artists, only: [:show, :update, :destroy]
+
+
+  def all_artists
+    render json: Artists.all
+  end
+
   def index
+    render json: current_user.artists
   end
 
   def show
+    render json: @artists
   end
 
-  def new
+  def create
+    render json: current_user.artists.create(artist_params)
   end
+
+  def update
+    @artist.update_attributes(artist_params)
+    render json: @artist
+  end
+
+  def destroy
+    @artist.destroy
+  end
+
+  private 
+
+  def set_artists
+    @artist = current_user.artists.find(params[:id])
+  end
+
+  def artist_params
+    params.require(:artist).permit(:albums, :genre)
+  end 
+
 end
--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,10 +1,39 @@
 class Api::MoviesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_movies, only: [:show, :update, :destroy]
+
+  def all_movies
+    render json: Movies.all
+  end
+
   def index
+    render json: current_user.movies
   end
 
   def show
+    render json: @movies
   end
 
-  def new
+  def create
+    render json: current_user.movies.create(movie_params)
+  end
+
+  def update
+    @movie.update_attributes(movie_params)
+    render json: @movie
+  end
+
+  def destroy
+    @movie.destroy
+  end
+
+  private 
+
+  def set_movies
+    @movie = current_user.movies.find(params[:id])
+  end
+
+  def movie_params
+      params.require(:movie).permit(:summary, :genre, :run_time, :rating)
   end
 end
