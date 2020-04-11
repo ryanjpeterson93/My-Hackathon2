1f903dd7220200f18bc1bb6debcb8ea45e01f2f0
Ryan Peterson <ryanjpeterson93@gmail.com>
2020-04-11 16:43:18 -0600

    a bit of styling for the movies

--- client/src/components/AllMovies.js
+++ client/src/components/AllMovies.js
@@ -1,50 +1,40 @@
-import React, { useState, useEffect, } from 'react';
-import axios from "axios"
-import Movie from './Movie';
-
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import { Card } from "semantic-ui-react";
 
 const AllMovies = () => {
   const [movies, setMovies] = useState([]);
-  
 
   useEffect(() => {
-    axios.get("/api/movies/all_movies")
-      .then(res => {
-        setMovies(res.data)
-        console.log(res.data)
-      })
+    axios.get("/api/movies/all_movies").then((res) => {
+      setMovies(res.data);
+      console.log(res.data);
+    });
   }, []);
 
-console.log(movies)
+  console.log(movies);
 
-const renderMovies = () =>{
-  return movies.map(movie =>(
-    <>
-    <div>
-    {movie.title}
-    {movie.summary}
-    {movie.genre}
-    {movie.run_time}
-    {movie.rating}
-    </div>
-    </>
-  ))
-}
-  // const renderMovies = () => {
-  //   return movies.map( movie => (
-      // <div key = {`movie-${movie.id}`}>
-      //  <Movie movie={movie} />
-    //   <div>
-  //   ))
-  // };
+  const renderMovies = () => {
+    return movies.map((movie) => (
+      <>
+        <Card>
+          <Card.Header>{movie.title}</Card.Header>
+          <Card.Meta>Summary: {movie.summary}</Card.Meta>
+          <Card.Meta>Genre: {movie.genre}</Card.Meta>
+          <Card.Content>Runtime: {movie.run_time}mins</Card.Content>
+          <hr />
+          <Card.Content>Rating {movie.rating}</Card.Content>
+        </Card>
+      </>
+    ));
+  };
 
   const deleteMovie = (id) => {
-    axios.delete(`/api/movies/${id}`)
-      .then( res => {
-        setMovies(movies.filter( (movie) => movie.id !== id))
-      })
-  }
+    axios.delete(`/api/movies/${id}`).then((res) => {
+      setMovies(movies.filter((movie) => movie.id !== id));
+    });
+  };
 
   return (
     <div>
